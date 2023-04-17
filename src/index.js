8bcf625ff7d048cfe425a0a248ba5d99038b496c
Alexandr Ivanov <Alexandr.ivanov.1024@gmail.com>
2023-04-17 22:28:41 +0300

    add index

--- src/index.js
+++ src/index.js
@@ -1,2 +1,27 @@
 /** @jsx h */
 let foo = <div id="foo">Hello!</div>;
+
+function h(nodeName, attributes, ...args) {
+  const children = args?.length ? [].concat(...args) : null;
+
+  return { nodeName, attributes, children }
+}
+
+function render(vNode) {
+  // String just convert to text #node
+  if (vNode.split) {
+    return document.createTextNode(vNode);
+  }
+
+  // create a Dom element with the nodeName of our VDOM element
+  const n = document.createElement(vNode.nodeName);
+
+  // copy attributes onto the new node
+  const a = vNode.attributes || {};
+  Object.keys(a).forEach(k => n.setAttributes(k, a[k]));
+
+  // render (build) and then append child nodes
+  (vNode.children || []).forEach(c => render(c));
+
+  return n;
+}
